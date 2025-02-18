280738b8961e66d459d217279799b5be98678f34
Eriwe <11926638+zakingslayerv22@users.noreply.github.com>
2025-02-18 04:26:20 -0800

    Implement set() on linked list
    Initialize a set() method
    that takes in a `key` and
    a `value`. Create a new Node
    and set the new Node as
    the head if there is no
    head.
    
    If the key of
    the head is the same as
    the key of this new node,
    have the value of this
    new node, replace the value
    of the head. If not, keep
    traversing the linked list
    and any Node that bears the
    same key as this new node
    will have its value replaced
    by the value of this new
    node.
    
    If none of this is the case,
    then append this new node
    as the last node.
    
    The value overwriting feature
    is to prevent nodes with same
    keys to be stored in the
    linked list (this is not a
    collision)
    and this feature
    is essential to hashmaps.
    
    Test this method in index.js
    and log the list to the
    console.

--- linked-lists.js
+++ linked-lists.js
@@ -2,6 +2,31 @@ export class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  set(key, value) {
+    const newNode = new Node(key, value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+
+      if (currentNode.key === key) {
+        currentNode.value = value;
+        return;
+      }
+
+      while (currentNode.nextNode) {
+        currentNode = currentNode.nextNode;
+        if (currentNode.key === key) {
+          currentNode.value = value;
+          return;
+        }
+        currentNode = currentNode.nextNode;
+      }
+      currentNode.nextNode = newNode;
+    }
+  }
 }
 
 class Node {
--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import { LinkedList } from "./linked-lists.js";
 
 const list = new LinkedList();
+
+list.set("One", "Norway");
+
+list.set("One", "Norway again");
+
+list.set("Two", "Sweden");
+
+console.log(list);
